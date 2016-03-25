3cc281191626c83e00e534528c851702fa08ec33
Kelly Setzer <setzer@liquidchicken.org>
2016-03-25 16:52:39 -0500

    Testing

--- lib/netaddr.rb
+++ lib/netaddr.rb
@@ -1,6 +1,6 @@
 =begin rdoc
  Copyleft (c) 2006 Dustin Spinhirne
-  
+
  Licensed under the same terms as Ruby, No Warranty is provided.
 =end
 
@@ -13,17 +13,24 @@ require File.join(File.dirname(__FILE__), 'methods.rb')
 require File.join(File.dirname(__FILE__), 'cidr.rb')
 require File.join(File.dirname(__FILE__), 'tree.rb')
 require File.join(File.dirname(__FILE__), 'eui.rb')
+require File.join(File.dirname(__FILE__), 'subnet.rb')
 
 module NetAddr
 
-class BoundaryError < StandardError #:nodoc:
-end
+  class Foo
+    def self.version
+      @version = '1.6.0'
+    end
+  end
 
-class ValidationError < StandardError #:nodoc:
-end
+  class BoundaryError < StandardError #:nodoc:
+  end
 
-class VersionError < StandardError #:nodoc:
-end
+  class ValidationError < StandardError #:nodoc:
+  end
+
+  class VersionError < StandardError #:nodoc:
+  end
 
 end # module NetAddr
 
